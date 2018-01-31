35096cbbb4469f1e44eea54af196310897f9ed46
Michael Wiebusch <m.wiebusch@gsi.de>
2018-01-31 15:31:14 +0100

    progress

--- MDC_II_sim.sh
+++ MDC_II_sim.sh
@@ -6,13 +6,13 @@ temp=$(mktemp)
 plot_out=mdc_$(./run_counter.sh).ps
 
 
-generator=./MDC_cells/gen_MDC_II_1700V_3x3x3.py
+cell_generator=./MDC_cells/gen_MDC_II_1700V_3x3x3.py
 
-view_cell=true
+# view_cell=true
 # plot_field=true
 # field_animation=true
+view_inner_cube=true
 
-animation=false
 
 
 
@@ -25,7 +25,7 @@ cat <<EOF >>$temp
 EOF
 
 ### generate cell
-$generator >>$temp
+$cell_generator >>$temp
 
 ### field section ###
 cat <<EOF >>$temp
@@ -46,17 +46,23 @@ area -1.2 -1.2 -1.2 1.2 1.2 1.2 ...
 Call plot_field_area
 Call plot_end
 
+//  plot cross section
+area -0.6 -0.6 -0.6 0.6 0.6 0.6 ...
+    view x=0.1 cut rot 90
+Call plot_field_area
+Call plot_end
+
+EOF
+fi
+
+### view inner cube ###
+if [ $view_inner_cube == "true" ]; then
+cat <<EOF >>$temp
 // View innermost cube
 area -0.4 -0.4 -0.4 0.4 0.4 0.4 ...
    nooutline ...
     light -20 -40 ...
     view -3*x+5*y-1*z=0 3d
-Call plot_field_area
-Call plot_end
-
-//  plot cross section
-area -0.6 -0.6 -0.6 0.6 0.6 0.6 ...
-    view x=0.1 cut rot 90
 Call plot_field_area
 Call plot_end
 
@@ -103,6 +109,17 @@ pl surf
 EOF
 done
 fi
+
+
+
+## no magnetic field
+cat <<EOF>>$temp
+*****************MAGNETIC*******************
+&magnetic
+components  0.00  0.00  0.00 T 
+
+EOF
+
 
 
 
